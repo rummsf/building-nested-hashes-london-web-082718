de5707fb67df8bdd7f08016e7ad045814fbb46ab
Irum F <irummf@gmail.com>
2018-08-27 01:43:01 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,14 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
+
       #your key/value pairs here!
       }, 
     :capulet => {
+           :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       #your key/value pairs here!
       }}
 
